3ff7ab4a13c83120c85753814214114c06af7a6e
Lukas Steiner <info@julbul.io>
2021-01-29 20:59:40 +0100

    init push

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+	purge: ['./src/**/*.{js, jsx, ts,tsx}', './public/index.html'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			fontFamily: {
+				scrptai: [
+					'Rubik',
+					'sans-serif',
+					// 'ui-serif',
+					// 'Georgia',
+					// 'Cambria',
+					// '"Times New Roman"',
+					// 'Times',
+					// 'serif'
+				],
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
