ae1dd3edbae4044279b2dc3d6b4fbc19e3c8fade
Lukas Steiner <info@julbul.io>
2021-02-12 00:50:14 +0100

    craco change

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+	addons: [
+		{
+		  name: "storybook-preset-craco",
+		  options: {
+			cracoConfigFile: "../craco.config.js",
+		  },
+		},
+	  ],
 	style: {
 		postcss: {
 			plugins: [require('tailwindcss'), require('autoprefixer')],
