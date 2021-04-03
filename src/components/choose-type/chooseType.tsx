752abda4fea9b11e4373962cd025943eefc6cc58
Lukas Steiner <info@julbul.io>
2021-04-04 01:41:54 +0200

    grid > flex (/generate types)

--- src/components/choose-type/chooseType.tsx
+++ src/components/choose-type/chooseType.tsx
@@ -52,7 +52,7 @@ const ChooseType: FC = (): ReactElement => {
 			icon: '💭',
 		},
 		{
-			text: 'Articlea Headlines',
+			text: 'Article Headlines',
 			description: 'Take an article you may have written and use it to translate the content into a headline.',
 			route: 'articleheadlines',
 			icon: '💯',
@@ -67,25 +67,22 @@ const ChooseType: FC = (): ReactElement => {
 
 	return (
 		<section className="flex-shrink flex flex-col items-center justify-center  h-full w-full">
-			<div className="flex flex-col w-3/4 h-1/5 justify-center ">
+			<div className="flex flex-col w-3/4 h-1/5 justify-center  max-w-screen-lg">
 				<h1 className="tracking-wide text-3xl text-gray-900 font-medium md:pl-20">Choose content type</h1>
 			</div>
-			<div className="flex flex-col w-3/4 h-4/5 items-center space-y-10 overflow-y-auto ">
+			<div className="flex md:grid flex-col md:space-y-0 w-3/4  md:grid-cols-3 md:grid-rows-3 md:gap-4  max-w-screen-md xl:max-w-screen-lg overflow-y-auto">
 				{types.map((item, index) => {
 					return (
 						<NavLink exact to={`/generate/${item.route}`} key={index}>
 							<div
-								className="flex flex-col space-y-4 bg-teal-50 rounded-lg capitalize 
-									  transition-all ease-in-out duration-200 text-lg md:text-xl font-medium tracking-wide
-									 focus:outline-none"
+								key={index}
+								className="flex flex-col space-y-3 items-center rounded-lg py-2 md:py-6 px-2  h-44 md:h-52 border-2 border-gray-100 hover:border-teal-700 duration-200 transition-all ease-in-out"
 							>
-								<div className="text-lg text-gray-900 tacking-wide">
-									<h1>{item.text}</h1>{' '}
-									<span role="image" aria-label={`${item.icon}`}>
-										{item.icon}
-									</span>
-								</div>
-								<p className="text-base text-gray-700">{item.description}</p>
+								<span role="image" aria-label={`${item.icon}`} className="font-emoji bg-teal-100 py-2 px-4 rounded-lg text-xl">
+									{item.icon}
+								</span>
+								<h1 className="text-gray-900 tracking-wide font-medium text-lg">{item.text}</h1>
+								<p className="text-gray-700 text-center">{item.description}</p>
 							</div>
 						</NavLink>
 					);
