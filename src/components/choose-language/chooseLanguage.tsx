cf57112df70f644e93e1971870d8124ba53c4b88
Lukas Steiner <info@julbul.io>
2021-04-04 17:41:43 +0200

    refactored /generate UI

--- src/components/choose-language/chooseLanguage.tsx
+++ src/components/choose-language/chooseLanguage.tsx
@@ -31,26 +31,35 @@ const ChooseLanguage: FC = (): ReactElement => {
 	return (
 		<section className="flex-shrink  flex flex-col items-center justify-center h-full w-full">
 			<div className="flex flex-col w-3/4 h-1/5 justify-center">
-				<h1 className="tracking-wide text-3xl text-gray-900 font-medium md:pl-20">Choose language</h1>
+				<h1 className="flex justify-center md:justify-start tracking-wide text-3xl text-gray-900 font-medium md:pl-20">Choose language</h1>
 			</div>
-			<div className="flex flex-col w-3/4 h-4/5 justify-start items-center space-y-10">
-				{languages.map((item, index) => {
-					return (
-						<NavLink exact to={`/generate/${type}/${item.language}`} key={index}>
-							<button
-								className="w-64 md:w-80 py-6  bg-teal-700 rounded-lg text-white capitalize hover:bg-teal-600
-									  transition-all ease-in-out duration-200 text-lg md:text-xl font-medium tracking-wide
-									 focus:outline-none"
-							>
-								<p>
-									<span className="pr-2 md:pr-4">{item.icon}</span>
-									{item.language}
-								</p>
-							</button>
-						</NavLink>
-					);
-				})}
-			</div>
+
+			<section className="flex justify-center md:justify-start w-3/4 h-4/5 overflow-y-auto pb-10 md:pl-20  max-w-screen-lg">
+				<div className="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:grid-rows-3 gap-4 w-3/4 md:w-full max-h-screen transition-all duration-150 ">
+					{languages.map((item, index) => {
+						return (
+							<NavLink exact to={`/generate/${type}/${item.language}`} key={index}>
+								<div
+									key={index}
+									className="flex flex-col justify-center space-y-1 lg:space-y-3 items-center rounded-lg py-2 md:py-6 px-2  h-36 lg:h-44  border-2 border-gray-100 hover:border-teal-700 duration-200 transition-all ease-in-out shadow-md"
+								>
+									<span
+										role="image"
+										aria-label={`${item.icon}`}
+										className="font-emoji bg-teal-100 py-2 px-3 lg:py-2 lg:px-4 rounded-lg text-lg lg:text-2xl"
+									>
+										{item.icon}
+									</span>
+									<h1 className="text-gray-900 tracking-wide font-medium  text-sm lg:text-base 2xl:text-lg text-center capitalize">
+										{item.language}
+									</h1>
+									{/* <p className="text-gray-700 text-center text-xs lg:text-sm 2xl:text-base">{item.description}</p> */}
+								</div>
+							</NavLink>
+						);
+					})}
+				</div>
+			</section>
 		</section>
 	);
 };
--- src/components/choose-type/chooseType.tsx
+++ src/components/choose-type/chooseType.tsx
@@ -68,26 +68,34 @@ const ChooseType: FC = (): ReactElement => {
 	return (
 		<section className="flex-shrink flex flex-col items-center justify-center  h-full w-full">
 			<div className="flex flex-col w-3/4 h-1/5 justify-center  max-w-screen-lg">
-				<h1 className="tracking-wide text-3xl text-gray-900 font-medium md:pl-20">Choose content type</h1>
+				<h1 className="flex justify-center md:justify-start tracking-wide text-3xl text-gray-900 font-medium md:pl-20 ">
+					Choose content type
+				</h1>
 			</div>
-			<div className="flex md:grid flex-col md:space-y-0 w-3/4  md:grid-cols-3 md:grid-rows-3 md:gap-4  max-w-screen-md xl:max-w-screen-lg overflow-y-auto">
-				{types.map((item, index) => {
-					return (
-						<NavLink exact to={`/generate/${item.route}`} key={index}>
-							<div
-								key={index}
-								className="flex flex-col space-y-3 items-center rounded-lg py-2 md:py-6 px-2  h-44 md:h-52 border-2 border-gray-100 hover:border-teal-700 duration-200 transition-all ease-in-out"
-							>
-								<span role="image" aria-label={`${item.icon}`} className="font-emoji bg-teal-100 py-2 px-4 rounded-lg text-xl">
-									{item.icon}
-								</span>
-								<h1 className="text-gray-900 tracking-wide font-medium text-lg">{item.text}</h1>
-								<p className="text-gray-700 text-center">{item.description}</p>
-							</div>
-						</NavLink>
-					);
-				})}
-			</div>
+			<section className="flex justify-center md:justify-start w-3/4 h-4/5 overflow-y-auto pb-10 md:pl-20  max-w-screen-lg">
+				<div className="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:grid-rows-3 gap-4 w-3/4 md:w-full h-screen transition-all duration-150 ">
+					{types.map((item, index) => {
+						return (
+							<NavLink exact to={`/generate/${item.route}`} key={index}>
+								<div
+									key={index}
+									className="flex flex-col justify-center space-y-1 lg:space-y-3 items-center rounded-lg py-2 md:py-6 px-2  h-36 lg:h-44  border-2 border-gray-100 hover:border-teal-700 duration-200 transition-all ease-in-out shadow-md"
+								>
+									<span
+										role="image"
+										aria-label={`${item.icon}`}
+										className="font-emoji bg-teal-100 py-2 px-3 lg:py-2 lg:px-4 rounded-lg text-base lg:text-lg 2xl:text-xl"
+									>
+										{item.icon}
+									</span>
+									<h1 className="text-gray-900 tracking-wide font-medium  text-sm lg:text-base 2xl:text-lg text-center">{item.text}</h1>
+									<p className="text-gray-700 text-center text-xs lg:text-sm 2xl:text-base">{item.description}</p>
+								</div>
+							</NavLink>
+						);
+					})}
+				</div>
+			</section>
 		</section>
 	);
 };
