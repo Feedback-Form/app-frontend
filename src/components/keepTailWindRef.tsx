58e7b005f297ef2b36a003d52cbee71b87299b73
Lukas Steiner <info@julbul.io>
2021-04-06 15:09:16 +0200

    ready for prod!

---
+++ src/components/keepTailWindRef.tsx
@@ -0,0 +1,33 @@
+import React, { FC, ReactElement } from 'react';
+
+const KeepTailWindRef: FC = (): ReactElement => {
+	return (
+		<div>
+			<span className="text-purple-800">purple</span>
+			<span className="text-yellow-800">yellow</span>
+			<span className="text-lime-800">lime</span>
+			<span className="text-cyan-800">cyan</span>
+			<span className="text-teal-800">teal</span>
+			<span className="text-indigo-800">indigo</span>
+			<span className="text-rose-800">rose</span>
+			<span className="text-emerald-800">emerald</span>
+			<span className="text-red-800">red</span>
+			<span className="text-blue-800">blue</span>
+			<span className="text-purple-800">purple</span>
+			<span className="text-gray-800">gray</span>
+			<span className="bg-purple-300">purple</span>
+			<span className="bg-yellow-300">yellow</span>
+			<span className="bg-lime-300">lime</span>
+			<span className="bg-cyan-300">cyan</span>
+			<span className="bg-teal-300">teal</span>
+			<span className="bg-indigo-300">indigo</span>
+			<span className="bg-rose-300">rose</span>
+			<span className="bg-emerald-300">emerald</span>
+			<span className="bg-red-300">red</span>
+			<span className="bg-blue-300">blue</span>
+			<span className="bg-purple-300">purple</span>
+			<span className="bg-gray-300">gray</span>
+		</div>
+	);
+};
+export default KeepTailWindRef;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,6 @@
 const colors = require('tailwindcss/colors');
 module.exports = {
-	// purge: ['./src/**/*.{js, jsx, ts,tsx}', './public/index.html'],
-	purge: {
-		content: ['./src/**/*.{js, jsx, ts,tsx}', './public/index.html'],
-
-		safelist: [
-			'text-purple-800',
-			'text-yellow-800',
-			'text-lime-800',
-			'text-cyan-800',
-			'text-teal-800',
-			'text-indigo-800',
-			'text-rose-800',
-			'text-emerald-800',
-			'text-red-800',
-			'text-blue-800',
-			'text-purple-800',
-			'text-gray-800',
-			'text-indigo-800',
-			'bg-purple-300',
-			'bg-yellow-300',
-			'bg-lime-300',
-			'bg-cyan-300',
-			'bg-teal-300',
-			'bg-indigo-300',
-			'bg-rose-300',
-			'bg-emerald-300',
-			'bg-red-300',
-			'bg-blue-300',
-			'bg-purple-300',
-			'bg-indigo-300',
-			'bg-gray-300',
-		],
-	},
+	purge: ['./src/**/*.{js, jsx, ts,tsx}', './public/index.html'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {
